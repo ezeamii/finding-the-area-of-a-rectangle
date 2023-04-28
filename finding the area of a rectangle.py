be12e17178174b2469bf1a098e9ba694c0b3a5a1
ezeamii <131669112+ezeamii@users.noreply.github.com>
2023-04-28 13:23:39 +0100

    Add files via upload

---
+++ finding the area of a rectangle.py
@@ -0,0 +1,7 @@
+# PROGRAM THAT FINDS THE AREA OF A RECTANGLE
+
+length = input("What is the length of the rectangle: ")
+breadth = input("What is the breadth of the rectangle: ")
+
+Area = float(length) * float(breadth)
+print("The area of this rectangle is ", Area, "cm^2")
